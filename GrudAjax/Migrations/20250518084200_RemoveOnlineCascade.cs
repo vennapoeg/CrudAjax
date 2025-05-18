dda154d14351786aeab0f4a7dd18c73a8fff89e8
Esbe <esbe.lacroix@gmail.com>
2025-05-18 12:13:02 +0300

    Fix country dropdown selection on Edit City view

---
+++ GrudAjax/Migrations/20250518084200_RemoveOnlineCascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrudAjax.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOnlineCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.DropForeignKey(
+            name: "FK_Cities_Countries_CountryId",
+            table: "CITIES");
+
+          migrationBuilder.AddForeignKey(
+            name: "FK_Cities_Countries_CountryId",
+            table: "CITIES",
+            column: "CountryId",
+            principalTable: "Countries",
+            principalColumn: "Id",
+            onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.DropForeignKey(
+            name: "FK_Cities_Countries_CountryId",
+            table: "CITIES");
+
+          migrationBuilder.AddForeignKey(
+            name: "FK_Cities_Countries_CountryId",
+            table: "CITIES",
+            column: "CountryId",
+            principalTable: "Countries",
+            principalColumn: "Id",
+            onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
