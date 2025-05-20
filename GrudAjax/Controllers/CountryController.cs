e1f9f7693e3e288fdccdf5d2b0b9f79fe8786cbb
Esbe <esbe.lacroix@gmail.com>
2025-05-20 20:00:12 +0300

    Country Modal List

--- GrudAjax/Controllers/CountryController.cs
+++ GrudAjax/Controllers/CountryController.cs
@@ -43,6 +43,15 @@ namespace GrudAjax.Controllers;
       return PartialView("_CreateModalForm", country);
     }
 
+    [HttpPost]
+
+    public IActionResult CreateModalForm(Country country)
+    {
+      _context.Add(country);
+      _context.SaveChanges();
+      return NoContent();
+    }
+
     [HttpGet]
     public IActionResult Details(int Id)
     {
@@ -101,6 +110,27 @@ namespace GrudAjax.Controllers;
         return View(country);
       }
       return RedirectToAction(nameof(Index));
+    }
+
+    public JsonResult GetCountries()
+    {
+      var lstCountries = new List<SelectListItem>();
+      List<Country> Countries = _context.Countries.ToList();
+      lstCountries = Countries.Select(ct => new SelectListItem()
+      {
+        Value = ct.Id.ToString(),
+        Text = ct.Name
+      }).ToList();
+
+      var defItem = new SelectListItem()
+      {
+        Value = "",
+        Text = "----Select Country----"
+      };
+
+      lstCountries.Insert(0, defItem);
+      return Json(lstCountries);
+
     }
 
 
--- GrudAjax/wwwroot/js/site.js
+++ GrudAjax/wwwroot/js/site.js
@@ -49,3 +49,39 @@ function ShowCreateModalForm() {
   $("#DivCreateDialogHolder").modal('show');
   return;
 }
+
+function submitModalForm() {
+  var btnSubmit = document.getElementById('btnSubmit');
+  btnSubmit.click();
+}
+
+function refreshCountryList() {
+  var btnBack =document.getElementById('dupBackBtn');
+  btnBack.click();
+  FillCountries("lstCountryId");
+}
+
+function FillCountries(lstCountryId) {
+  var lstCountries = $("#"+lstCountryId);
+  lstCountries.empty();
+
+  lstCountries.append($('<option/>',
+    {
+      value: null,
+      text: "Select Country"
+    }))
+
+  $.getJSON("/country/GetCountries", function (countries){
+    if (countries != null && !jQuery.isEmptyObject(countries)) {
+      $.each(countries, function (index,country) {
+        lstCountries.append($('<option/>',
+          {
+            value: country.value,
+            text: country.text
+          }));
+      });
+    };
+  });
+
+  return;
+}
